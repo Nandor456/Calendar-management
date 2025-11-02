d57fcd222f4b80bd91819a631807eaa386907ecc
Mezei Nándor <mezeinandor23@gmail.com>
2025-11-02 23:23:01 +0200

    logger added

--- src/main/java/edu/bbte/idde/mnim2377/data/dao/DaoFactory.java
+++ src/main/java/edu/bbte/idde/mnim2377/data/dao/DaoFactory.java
@@ -1,19 +1,21 @@
 package edu.bbte.idde.mnim2377.data.dao;
 
 import edu.bbte.idde.mnim2377.config.DatabaseConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public abstract class DaoFactory {
     private static DaoFactory instance;
+    private static final Logger logger = LoggerFactory.getLogger(DaoFactory.class);
 
     public static DaoFactory getInstance() {
         if (instance == null) {
             String daoType = DatabaseConfig.getDaoType();
-            System.out.println("DAO type from config: " + daoType);
             if (daoType.equalsIgnoreCase("jdbc")) {
-                System.out.println("Using PostgresCalendarDaoFactory");
+                logger.info("working with Postgres DAO");
                 instance = new PostgresCalendarDaoFactory();
             } else {
-                System.out.println("Using InMemoryCalendarDaoFactory");
+                logger.info("working with In-Memory DAO");
                 instance = new InMemoryCalendarDaoFactory();
             }
         }
